6ab65ef77d8503920175d672a5399d09ef992ff2
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-10 14:39:51 +0900

    feat: name 정보를 GET Query parameter로 받아 다시 보여주는 예제

--- ping_pong/views.py
+++ ping_pong/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def ping(request):
-    return render(request, 'ping.html')
+    name = request.GET.get('name', 'No Name')
+    return render(request, 'ping.html', { 'name': name })
