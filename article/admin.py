8b0b70b1484d016cda0b0b3fe9f9f2cc530bca68
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-12 14:05:50 +0900

    feat: Admin에서 Article 모델 확인해보기

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'author'
+    ]
+    list_filter = [
+        'title',
+        'author'
+    ]
+    list_per_page = 10
+
