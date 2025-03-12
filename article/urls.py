bf20e9543ea6e2f1a1ab4953cfd3643d464e656e
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-12 17:18:44 +0900

    feat: (응용) edit, update와 같이 꼭 2개의 view가 있어야할까?

--- article/urls.py
+++ article/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import index, show, new, create, edit, update, delete, create_comment
+from .views import index, show, create, update, delete, create_comment
 
 app_name = 'article'
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:pk>', show, name='show'),
-    path('new/', new, name='new'),
     path('create/', create, name='create'),
-    path('<int:pk>/edit/', edit, name='edit'),
     path('<int:pk>/update/', update, name='update'),
     path('<int:pk>/delete/', delete, name='delete'),
     path('<int:article_id>/comments/create_comment/', create_comment, name='create_comment'),
--- article/views.py
+++ article/views.py
@@ -16,32 +16,29 @@ def show(request, pk):
     return render(request, 'show.html', { 'article': article, 'comments': comments })
 
 
-def new(request):
-    return render(request, 'new.html')
-
 
 def create(request):
-    article = Article()
-    article.author = request.user
-    article.title = request.POST['title']
-    article.content = request.POST['content']
-    article.save()
-
-    return redirect('article:show', pk=article.id)
-
-
-def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-
-    return render(request, 'edit.html', { 'article': article })
+    if request.method == 'POST':
+        article = Article()
+        article.author = request.user
+        article.title = request.POST['title']
+        article.content = request.POST['content']
+        article.save()
+        return redirect('article:show', pk=article.id)
+    else:
+        return render(request, 'new.html')
 
 
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    article.title = request.POST['title']
-    article.content = request.POST['content']
-    article.save()
-    return redirect('article:show', pk=article.id)
+
+    if request.method == 'POST':
+        article.title = request.POST['title']
+        article.content = request.POST['content']
+        article.save()
+        return redirect('article:show', pk=article.id)
+    else:
+        return render(request, 'edit.html', { 'article': article })
 
 
 def delete(request, pk):
