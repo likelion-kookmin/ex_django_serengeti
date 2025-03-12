9d42478cd9c5779f6a72b2b4b8e3859a68645f8d
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-12 14:06:21 +0900

    feat: Admin에서 Comment 모델 확인해보기

--- article/admin.py
+++ article/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from article.models import Article
+from article.models import Article, Comment
 
 
 @admin.register(Article)
@@ -16,3 +16,12 @@ class ArticleAdmin(admin.ModelAdmin):
     ]
     list_per_page = 10
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'article',
+        'author',
+    ]
+    list_per_page = 10
